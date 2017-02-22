041a6060f390a1fff0a23906064ee5ab2425e9fb
abelflz <abelfeliz11@gmail.com>
2017-02-21 21:27:28 -0400

    Se agregó LogIn y se definió la entidad Usuario

--- Proyecto Propietaria II/Controllers/HomeController.cs
+++ Proyecto Propietaria II/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
 
 namespace Proyecto_Propietaria_II.Controllers
 {
@@ -11,6 +12,33 @@ namespace Proyecto_Propietaria_II.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost, ActionName("Index")]
+        public ActionResult IndexPost(string UserName, string Password)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=ABEL-PC;Initial Catalog=Banking;Integrated Security=True";
+            con.Open();
+
+            string query = "select COUNT(Usuario) from LogIn where Usuario = @a and Contrasena = @b";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.Add(new SqlParameter("@a", UserName));
+            cmd.Parameters.Add(new SqlParameter("@b", Password));
+
+            int verificar = Convert.ToInt32(cmd.ExecuteScalar());
+
+            if (verificar == 1)
+            {
+                ViewBag.Hello = UserName;
+                con.Close();
+                return PartialView("NewNavBar");
+            }
+            else
+            {
+                ViewBag.Error = "Ta Forzando";
+                return View("~/Views/Shared/Error.cshtml");
+            }  
         }
 
         public ActionResult About()
@@ -22,6 +50,7 @@ namespace Proyecto_Propietaria_II.Controllers
 
         public ActionResult Contact()
         {
+            //ADO.NET Stuff
             ViewBag.Message = "Your contact page.";
 
             return View();
---
+++ Proyecto Propietaria II/Models/Usuario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Propietaria_II.Models
+{
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required(ErrorMessage = "Debe digitar una cédula")]
+        [StringLength(11)]
+        public int Cedula { get; set; }
+
+        [StringLength(30)]
+        public string Nombre { get; set; }
+
+        [StringLength(30)]
+        public string Apellido { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "Correo no válido")]
+        public string Correo { get; set; }
+
+        [StringLength(100)]
+        public string Direccion { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
